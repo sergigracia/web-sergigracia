3695682bab26554ccbc9b98e59e7062e07926639
Sergi Gracia <sergigram@gmail.com>
2017-10-23 12:23:52 +0200

    Add semantic-ui css and gravatar package

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {
+  Segment,
+  Container,
+  Header,
+} from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Segment
+        textAlign='center'
+        style={{ minHeight: 700, padding: '1em 0em' }}
+        vertical
+      >
+        <Container text>
+          <Gravatar email="sergigram@gmail.com" size={100} rating="pg" default="monsterid" className="CustomAvatar-image" />
+          <Header as='h3' style={{ fontSize: '2em' }}>Sergi Gracia</Header>
+          <p style={{ fontSize: '1.33em' }}>
+            We can give your company superpowers to do things that they never thought possible. Let us delight
+              </p>
+          <p style={{ fontSize: '1.33em' }}>
+            I'm Sergi Gracia a Catalan developer currently living in Barcelona.
+              </p>
+          <p style={{ fontSize: '1.33em' }}>
+            Former iOS Developer at BlooSee, Redbooth, Wallapop and other companies I'm not allowed to <strike>name</strike>.
+              </p>
+          <p style={{ fontSize: '1.33em' }}>
+            Currently building Mobile, TV and Watch experiences at <a class="color3" href="http://caramba.io">caramba.io</a>
+          </p>
+          <p style={{ fontSize: '1.33em' }}>
+            Contact me at <a class="color4" href="mailto:sergi@caramba.io">sergi@caramba.io</a>
+          </p>
+        </Container>
+      </Segment>
     );
   }
 }
